abb06c4cd9db0170161a42fcb511fbed06d23ff3
magrossmann <maxi.grossmann@hotmail.com>
2021-03-14 00:14:14 +0100

    Add ButtonEventlistener/PrimCalculation(2.2)

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendReq = (Button)findViewById(R.id.SendRequestButton);
+        sendReq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userInp = (EditText)findViewById(R.id.UserNumberInput);
+
+            }
+        });
+        Button calcPrim = (Button)findViewById(R.id.CalculatePrimButton);
+        calcPrim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText calcNumber = (EditText)findViewById(R.id.UserNumberInput);
+                String numToCalc = calcNumber.getText().toString();
+                String result = "";
+                for (int i = 0; i < numToCalc.length(); i++) {
+                    if (numToCalc.charAt(i) == '2' ||numToCalc.charAt(i) == '3' ||numToCalc.charAt(i) == '5' ||numToCalc.charAt(i) == '7') {
+                        result += numToCalc.charAt(i);
+                    }
+                }
+                TextView resText = (TextView)findViewById(R.id.ServerResponse);
+                resText.setText(result.toString());
+            }
+        });
+
     }
 }
